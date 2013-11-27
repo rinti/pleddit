5023805748b8d0e1078d8cc4fe05ae63c168fa02
Andreas Bernacca <ante.bernacca@gmail.com>
2013-11-27 15:59:01 +0100

    flask stuff

--- pleddit.py
+++ pleddit.py
@@ -1,7 +1,14 @@
 from flask import Flask
-from flask import render_template, url_for
+from flask import render_template
+from werkzeug.contrib.fixers import ProxyFix
+
 app = Flask(__name__)
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
