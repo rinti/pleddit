c1fa68d8d6d154ddf3a713810e4e1eeea45f7ee3
rinti <andreas@rinti.se>
2019-02-19 22:32:01 +0100

    Add initial index view with state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+const Song = ({ title, url }) => {
+    return <li>{title}</li>
+}
+
 const App = () => {
+    const [subreddit, setSubreddit] = useState('')
+    const [songs, setSongs] = useState([])
+
+    const handleSubredditChange = (e) => {
+        setSubreddit(e.target.value)
+    }
+
+    const handleFetchClick = (e) => {
+        setSongs([{title: 'test'}])
+    }
+
     return (
-      <div className="App">
-      </div>
+        <div className="App">
+            <section>
+                <input type="text" value={subreddit} onChange={handleSubredditChange} />
+                <button onClick={handleFetchClick}>
+                    fetchy
+                </button>
+            </section>
+            <ul>
+                {songs.map(item => <Song {...item} />)}
+            </ul>
+        </div>
     );
 }
 
